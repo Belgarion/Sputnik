97a59ec969fdf75d0225fdf5ce1da7781818a098
andrag <andrag-0@student.ltu.se>
2014-05-26 10:47:00 +0200

    hej

--- src/main/Main.java
+++ src/main/Main.java
@@ -156,7 +156,7 @@ public class Main extends SimpleApplication {
 		threads = new Vector<Thread>();
 		try {
 			//System.out.println("bajs");
-			client = new Client("130.240.109.113", 12345);
+			client = new Client("130.240.94.213", 12345);
 			NetworkRecvThread recvThread = new NetworkRecvThread(state, client);
 			NetworkSendThread sendThread = new NetworkSendThread(state, client);
 			threads.add(recvThread);
